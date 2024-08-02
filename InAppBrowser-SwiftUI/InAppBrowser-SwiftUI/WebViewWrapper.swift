c644239e1c9c1b05361c9204fa1e03ca4353c484
Diego dos Santos <11074197+diegodossantos95@users.noreply.github.com>
2024-08-02 10:43:06 -0300

    Create Coordinator for WebViewWrapper

--- InAppBrowser-SwiftUI/InAppBrowser-SwiftUI/WebViewWrapper.swift
+++ InAppBrowser-SwiftUI/InAppBrowser-SwiftUI/WebViewWrapper.swift
@@ -8,15 +8,75 @@
 import SwiftUI
 import WebKit
 
-public struct WebViewWrapper: UIViewRepresentable {
+struct WebViewWrapper: UIViewRepresentable {
     var url: URL
 
-    public func makeUIView(context: Context) -> WKWebView {
+    func makeUIView(context: Context) -> WKWebView {
         let webView = WKWebView()
         let request = URLRequest(url: url)
+        context.coordinator.webView = webView
         webView.load(request)
         return webView
     }
 
-    public func updateUIView(_ uiView: WKWebView, context: Context) { }
+    func updateUIView(_ uiView: WKWebView, context: Context) { }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+}
+
+extension WebViewWrapper {
+    class Coordinator: NSObject, WKNavigationDelegate {
+
+        private var loadingObserver: NSKeyValueObservation?
+        private var loadingProgressObserver: NSKeyValueObservation?
+        private var goBackObserver: NSKeyValueObservation?
+        private var goForwardObserver: NSKeyValueObservation?
+        private var pageTitleObserver: NSKeyValueObservation?
+       
+        weak var webView: WKWebView? {
+            didSet {
+                webView?.navigationDelegate = self
+            }
+        }
+
+        func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+            setObservers()
+        }
+
+        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+            handleError(error)
+        }
+
+        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+            handleError(error)
+        }
+
+        private func setObservers() {
+            loadingObserver = webView?.observe(\.isLoading) { webView, _ in
+                print("isLoading: \(webView.isLoading)")
+            }
+
+            loadingProgressObserver = webView?.observe(\.estimatedProgress)  { webView, _ in
+                print("estimatedProgress: \(Float(webView.estimatedProgress))")
+            }
+
+            goBackObserver = webView?.observe(\.canGoBack) { webView, _ in
+                print("canGoBack: \(webView.canGoBack)")
+            }
+
+            goForwardObserver = webView?.observe(\.canGoForward) { webView, _ in
+                print("canGoForward: \(webView.canGoForward)")
+            }
+
+            pageTitleObserver = webView?.observe(\.title) { webView, _ in
+                print("title: \(webView.title ?? "")")
+            }
+        }
+
+        private func handleError(_ error: Error) {
+            print("error: \(error)")
+        }
+    }
 }
