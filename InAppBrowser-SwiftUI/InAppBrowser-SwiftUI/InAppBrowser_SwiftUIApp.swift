24ac1da1079bf63829dc7a5450448b846cdcefed
Diego dos Santos <11074197+diegodossantos95@users.noreply.github.com>
2024-08-02 09:45:21 -0300

    Create base project

---
+++ InAppBrowser-SwiftUI/InAppBrowser-SwiftUI/InAppBrowser_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InAppBrowser_SwiftUIApp.swift
+//  InAppBrowser-SwiftUI
+//
+//  Created by Diego Dos Santos on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct InAppBrowser_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
