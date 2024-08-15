1a0e9157dde826e9cc06261d038639079bc3e274
Diego dos Santos <diego.dossantos@mightynetworks.com>
2024-08-15 14:52:13 -0300

    Create view model to handle navigation and webview state

--- InAppBrowser-SwiftUI/InAppBrowser-SwiftUI/WebViewModel.swift
+++ InAppBrowser-SwiftUI/InAppBrowser-SwiftUI/WebViewModel.swift
@@ -6,11 +6,92 @@
 //
 
 import Foundation
+import WebKit
 
-class WebViewModel: ObservableObject {
+class WebViewModel: NSObject, ObservableObject, WKNavigationDelegate {
+
+    // MARK: -  View state variables
+
     @Published var isLoading: Bool = false
     @Published var loadingProgress: Float = 0
     @Published var canGoBack: Bool = false
     @Published var canGoForward: Bool = false
     @Published var title: String = ""
+
+    // MARK: - WebView var
+
+    weak var webView: WKWebView? {
+        didSet {
+            webView?.navigationDelegate = self
+        }
+    }
+
+    // MARK: - Private observers
+
+    private var loadingObserver: NSKeyValueObservation?
+    private var loadingProgressObserver: NSKeyValueObservation?
+    private var goBackObserver: NSKeyValueObservation?
+    private var goForwardObserver: NSKeyValueObservation?
+    private var pageTitleObserver: NSKeyValueObservation?
+
+    // MARK: - Public navigation funcs
+
+    func goBack() {
+        webView?.goBack()
+    }
+
+    func goForward() {
+        webView?.goForward()
+    }
+
+    func reload() {
+        webView?.reload()
+    }
+
+    func openInExternalBrowser() {
+        guard let url = webView?.url else { return }
+        UIApplication.shared.open(url)
+    }
+
+    // MARK: - WKNavigationDelegate
+
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        setObservers()
+    }
+
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        handleError(error)
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        handleError(error)
+    }
+
+    // MARK: - Private funcs
+
+    private func setObservers() {
+        loadingObserver = webView?.observe(\.isLoading) { [weak self] webView, _ in
+            self?.isLoading = webView.isLoading
+        }
+
+        loadingProgressObserver = webView?.observe(\.estimatedProgress)  { [weak self] webView, _ in
+            self?.loadingProgress = Float(webView.estimatedProgress)
+        }
+
+        goBackObserver = webView?.observe(\.canGoBack) { [weak self] webView, _ in
+            self?.canGoBack = webView.canGoBack
+        }
+
+        goForwardObserver = webView?.observe(\.canGoForward) { [weak self] webView, _ in
+            self?.canGoForward = webView.canGoForward
+        }
+
+        pageTitleObserver = webView?.observe(\.title) { [weak self] webView, _ in
+            self?.title = webView.title ?? ""
+        }
+    }
+
+    private func handleError(_ error: Error) {
+        print("error: \(error)")
+    }
 }
--- InAppBrowser-SwiftUI/InAppBrowser-SwiftUI/WebViewWrapper.swift
+++ InAppBrowser-SwiftUI/InAppBrowser-SwiftUI/WebViewWrapper.swift
@@ -9,80 +9,21 @@ import SwiftUI
 import WebKit
 
 struct WebViewWrapper: UIViewRepresentable {
+
+    // MARK: -  Public vars
+
     var url: URL
     @ObservedObject var viewModel: WebViewModel
+
+    // MARK: -  UIViewRepresentable
 
     func makeUIView(context: Context) -> WKWebView {
         let webView = WKWebView()
         let request = URLRequest(url: url)
-        context.coordinator.webView = webView
+        viewModel.webView = webView
         webView.load(request)
         return webView
     }
 
     func updateUIView(_ uiView: WKWebView, context: Context) { }
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator(viewModel: viewModel)
-    }
-}
-
-extension WebViewWrapper {
-    class Coordinator: NSObject, WKNavigationDelegate {
-
-        @ObservedObject private var viewModel: WebViewModel
-        private var loadingObserver: NSKeyValueObservation?
-        private var loadingProgressObserver: NSKeyValueObservation?
-        private var goBackObserver: NSKeyValueObservation?
-        private var goForwardObserver: NSKeyValueObservation?
-        private var pageTitleObserver: NSKeyValueObservation?
-       
-        weak var webView: WKWebView? {
-            didSet {
-                webView?.navigationDelegate = self
-            }
-        }
-
-        init(viewModel: WebViewModel) {
-            self.viewModel = viewModel
-        }
-
-        func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
-            setObservers()
-        }
-
-        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
-            handleError(error)
-        }
-
-        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-            handleError(error)
-        }
-
-        private func setObservers() {
-            loadingObserver = webView?.observe(\.isLoading) { [weak self] webView, _ in
-                self?.viewModel.isLoading = webView.isLoading
-            }
-
-            loadingProgressObserver = webView?.observe(\.estimatedProgress)  { [weak self] webView, _ in
-                self?.viewModel.loadingProgress = Float(webView.estimatedProgress)
-            }
-
-            goBackObserver = webView?.observe(\.canGoBack) { [weak self] webView, _ in
-                self?.viewModel.canGoBack = webView.canGoBack
-            }
-
-            goForwardObserver = webView?.observe(\.canGoForward) { [weak self] webView, _ in
-                self?.viewModel.canGoForward = webView.canGoForward
-            }
-
-            pageTitleObserver = webView?.observe(\.title) { [weak self] webView, _ in
-                self?.viewModel.title = webView.title ?? ""
-            }
-        }
-
-        private func handleError(_ error: Error) {
-            print("error: \(error)")
-        }
-    }
 }
